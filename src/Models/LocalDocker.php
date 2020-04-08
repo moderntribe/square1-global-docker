bd42549283742bb7ab3f672e0ccd26ced206ad36
defunctl <justin.frydman@gmail.com>
2020-04-08 00:51:25 -0600

    Add comments

--- src/Models/LocalDocker.php
+++ src/Models/LocalDocker.php
@@ -2,11 +2,31 @@
 
 namespace Tribe\Sq1\Models;
 
+/**
+ * LocalDocker Model
+ *
+ * @package Tribe\Sq1\Models
+ */
 class LocalDocker {
 
+	/**
+	 * The Project's root directory.
+	 */
 	public const CONFIG_PROJECT_ROOT   = 'project_root';
+
+	/**
+	 * The Project's name, as found in dev/docker/.projectID
+	 */
 	public const CONFIG_PROJECT_NAME   = 'project_name';
+
+	/**
+	 * The Project's docker directory.
+	 */
 	public const CONFIG_DOCKER_DIR     = 'docker_dir';
+
+	/**
+	 * The Project's path to docker-compose.yml.
+	 */
 	public const CONFIG_DOCKER_COMPOSE = 'docker_compose';
 
 }
