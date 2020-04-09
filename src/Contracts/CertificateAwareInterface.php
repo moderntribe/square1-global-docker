ea1837389448f4838891683d54c84ca45cbd0c84
defunctl <justin.frydman@gmail.com>
2020-04-08 23:46:43 -0600

    Add CertificateAwareInterface.php

---
+++ src/Contracts/CertificateAwareInterface.php
@@ -0,0 +1,29 @@
+<?php declare( strict_types=1 );
+
+namespace Tribe\Sq1\Contracts;
+
+use Tribe\Sq1\Models\Certificate;
+
+/**
+ * Interface CertificateAwareInterface
+ *
+ * @package Tribe\Sq1\Contracts
+ */
+interface CertificateAwareInterface {
+
+	/**
+	 * Inject a certificate model.
+	 *
+	 * @param  \Tribe\Sq1\Models\Certificate  $cert
+	 *
+	 * @return void
+	 */
+	public function setCertificate( Certificate $cert ): void;
+
+	/**
+	 * Get the Certificate model.
+	 *
+	 * @return \Tribe\Sq1\Models\Certificate
+	 */
+	public function getCertificate(): Certificate;
+}
