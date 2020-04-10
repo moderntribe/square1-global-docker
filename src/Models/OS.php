8ab0fe825d68d353b970c8253b94498c75000ba1
defunctl <justin.frydman@gmail.com>
2020-04-10 12:55:42 -0600

    Add OS model

---
+++ src/Models/OS.php
@@ -0,0 +1,16 @@
+<?php declare( strict_types=1 );
+
+namespace Tribe\Sq1\Models;
+
+/**
+ * Operating System Model.
+ *
+ * @package Tribe\Sq1\Models
+ */
+class OS {
+
+	public const MAC_OS  = 'Darwin';
+	public const LINUX   = 'Linux';
+	public const WINDOWS = 'Windows';
+
+}
