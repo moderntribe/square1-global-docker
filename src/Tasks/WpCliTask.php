6e726fdfb179cb99816b3b2daab32e93c4e3ae0c
defunctl <justin.frydman@gmail.com>
2020-04-10 22:45:11 -0600

    Move WP command to use the --xdebug option

--- src/Tasks/WpCliTask.php
+++ src/Tasks/WpCliTask.php
@@ -16,29 +16,43 @@ class WpCliTask extends LocalDockerTask {
 	 * Run WP CLI commands in the Local Container.
 	 *
 	 * @command wp
+	 * @option  xdebug Run with Xdebug enabled.
+	 * @usage   e.g. sq1 wp -- option get home --format=json
 	 *
 	 * @param  array  $args  The WP CLI command and arguments.
-	 *
+	 * @param  array  $opts  The options.
 	 */
-	public function wp( array $args ) {
+	public function wp( array $args, array $opts = [ 'xdebug' => false ] ) {
+		$command = trim( implode( ' ', $args ) );
+
+		if ( $opts['xdebug'] ) {
+			$this->runWpX( $command );
+		} else {
+			$this->runWp( $command );
+		}
+	}
+
+	/**
+	 * Run WP CLI commands in the Local Container.
+	 *
+	 * @param  string  $command
+	 */
+	protected function runWp( string $command ): void {
 		$this->taskDockerComposeExecute()
 		     ->files( Robo::config()->get( LocalDocker::CONFIG_DOCKER_COMPOSE ) )
 		     ->projectName( Robo::config()->get( LocalDocker::CONFIG_PROJECT_NAME ) )
 		     ->setContainer( 'php-fpm' )
 		     ->envVariable( 'WP_CLI_PHP_ARGS', '' )
-		     ->exec( sprintf( 'wp --allow-root %s', trim( implode( ' ', $args ) ) ) )
+		     ->exec( sprintf( 'wp --allow-root %s', $command ) )
 		     ->run();
 	}
 
 	/**
 	 * Run WP CLI commands in the Local Container with Xdebug enabled.
 	 *
-	 * @command wpx
-	 *
-	 * @param  array  $args  The WP CLI command and arguments.
-	 *
+	 * @param  string  $command
 	 */
-	public function wpx( array $args ) {
+	protected function runWpX( string $command ) {
 		$projectName = Robo::config()->get( LocalDocker::CONFIG_PROJECT_NAME );
 
 		$this->taskDockerComposeExecute()
@@ -47,7 +61,7 @@ class WpCliTask extends LocalDockerTask {
 		     ->setContainer( 'php-fpm' )
 		     ->envVariable( 'PHP_IDE_CONFIG', "serverName=${projectName}.tribe" )
 		     ->exec( sprintf( 'php -dxdebug.remote_autostart=1 -dxdebug.remote_host=host.tribe -dxdebug.remote_enable=1 /usr/local/bin/wp --allow-root %s',
-			     trim( implode( ' ', $args ) ) ) )
+			     $command ) )
 		     ->run();
 	}
 
