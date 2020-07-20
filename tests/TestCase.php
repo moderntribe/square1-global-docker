6b6ebd094f59ce37bea94d1b38fa56eaf659c4d4
defunctl <justin.frydman@gmail.com>
2020-07-19 20:14:32 -0600

    Bootstrap SO in TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,21 @@
 namespace Tests;
 
 use Exception;
+use App\Bootstrap;
 use Illuminate\Support\Facades\Storage;
 use LaravelZero\Framework\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void {
+        parent::setUp();
+
+        // Bootstrap SO
+        $bootstrap = $this->app->make( Bootstrap::class );
+        $bootstrap->boot();
+    }
 
     public function tearDown(): void {
         parent::tearDown();
