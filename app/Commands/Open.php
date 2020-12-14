38441687c5caee179e55ef3d5d60755bb06628a8
defunctl <justin.frydman@gmail.com>
2020-12-13 23:58:41 -0700

    Allow `so open` to automatically launch a SquareOne project when no URL is provided

--- app/Commands/Open.php
+++ app/Commands/Open.php
@@ -2,7 +2,9 @@
 
 namespace App\Commands;
 
+use Exception;
 use App\Services\BrowserLauncher;
+use App\Services\Docker\Local\Config;
 
 /**
  * Open command
@@ -16,24 +18,40 @@ class Open extends BaseCommand {
      *
      * @var string
      */
-    protected $signature = 'open {url : The URL to open}';
+    protected $signature = 'open {url? : The URL to open}';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Open\'s a URL in your default browser';
+    protected $description = 'Open\'s a URL in your default browser or the current SquareOne project';
 
     /**
      * Execute the console command.
      *
-     * @param  \App\Services\BrowserLauncher  $launcher
+     * @param   \App\Services\BrowserLauncher  $launcher
+     * @param   Config                         $config
      *
-     * @return void
+     * @return int
      */
-    public function handle( BrowserLauncher $launcher ): void {
-        $launcher->open( $this->argument( 'url' ) );
+    public function handle( BrowserLauncher $launcher, Config $config ): int {
+        $url = $this->argument( 'url' );
+
+        if ( empty( $url ) ) {
+            try {
+                $url = $config->getProjectUrl();
+            } catch ( Exception $e ) {
+                $this->error( 'Please provide a valid URL or ensure this is run inside a SquareOne project' );
+
+                return self::EXIT_ERROR;
+            }
+
+        }
+
+        $launcher->open( $url );
+
+        return self::EXIT_SUCCESS;
     }
 
 }
