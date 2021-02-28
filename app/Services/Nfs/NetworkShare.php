265283bda4fddb75b52ced4a5a6b8a33d459b3c2
defunctl <justin.frydman@gmail.com>
2021-02-28 14:00:39 -0700

    WIP for a service to enable NFS shares on macOS

---
+++ app/Services/Nfs/NetworkShare.php
@@ -0,0 +1,126 @@
+<?php declare( strict_types=1 );
+
+namespace App\Services\Nfs;
+
+use App\Contracts\Runner;
+use App\Services\Docker\Network;
+use App\Exceptions\SystemExitException;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * Manage NFS shares.
+ *
+ * @package App\Services\Nfs
+ */
+class NetworkShare {
+
+    public const EXPORTS_FILE_PATH = '/etc/exports';
+
+    /**
+     * The docker network instance.
+     *
+     * @var \App\Services\Docker\Network
+     */
+    protected $network;
+
+    /**
+     * The Symfony filesystem.
+     *
+     * @var \Symfony\Component\Filesystem\Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * The command runner.
+     *
+     * @var \App\Contracts\Runner
+     */
+    protected $runner;
+
+    /**
+     * The NFS validator.
+     *
+     * @var \App\Services\Nfs\NfsValidator
+     */
+    protected $validator;
+
+    /**
+     * Controls the nfsd binary.
+     *
+     * @var \App\Services\Nfs\Nfsd
+     */
+    protected $nfsd;
+
+    /**
+     * NFS Share constructor.
+     *
+     * @param  \App\Services\Docker\Network              $network
+     * @param  \Symfony\Component\Filesystem\Filesystem  $filesystem
+     * @param  \App\Contracts\Runner                     $runner
+     * @param  \App\Services\Nfs\NfsValidator            $validator
+     * @param  \App\Services\Nfs\Nfsd                    $nfsd
+     */
+    public function __construct( Network $network, Filesystem $filesystem, Runner $runner, NfsValidator $validator, Nfsd $nfsd ) {
+        $this->network    = $network;
+        $this->filesystem = $filesystem;
+        $this->runner     = $runner;
+        $this->validator  = $validator;
+        $this->nfsd       = $nfsd;
+    }
+
+    /**
+     * Adds an NFS share.
+     *
+     * @param  string  $directory The directory to share.
+     *
+     * @throws \App\Exceptions\SystemExitException
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function add( string $directory ): void {
+
+        if ( ! $this->filesystem->exists( $directory ) ) {
+            throw new SystemExitException( sprintf( 'Unable to find path: %s', $directory ) );
+        }
+
+        $this->validator->validate( self::EXPORTS_FILE_PATH );
+
+        // @TODO break this up and move elsewhere to make it testable.
+        $open    = '#SquareOneStart';
+        $close   = '#SquareOneEnd';
+        $regex   = "/${open}[\s\S]+?${close}/";
+        $exports = $existing_exports = file_get_contents( self::EXPORTS_FILE_PATH );
+        $exports = preg_replace( $regex, '', $exports );
+
+        $exports .= "${open}\n";
+        $exports .= sprintf( "%s %s -alldirs -maproot=0:0\n", $directory, $this->network->getGateWayIP() );
+        $exports .= "${close}\n";
+
+        // No changes to the configuration
+        if ( $exports === $existing_exports ) {
+            return;
+        }
+
+        $tmpFile = $this->filesystem->tempnam( '/tmp', 'exports' );
+
+        $this->filesystem->dumpFile( $tmpFile, $exports );
+        $this->validator->checkExports( $tmpFile );
+        $this->filesystem->remove( $tmpFile );
+
+        // If we made it this far without thrown exceptions, write the exports configuration.
+        $this->runner->with( [ 'path' => self::EXPORTS_FILE_PATH, 'content' => $exports ] )
+                     ->run( 'echo -e "{{ $content }}" | sudo tee /etc/exports' )
+                     ->throw();
+
+        $this->nfsd->restart();
+    }
+
+    /**
+     * @TODO add remove share functionality.
+     *
+     * @param  string  $directory
+     */
+    public function remove( string $directory ): void {
+
+    }
+
+}
---
+++ app/Services/Nfs/NfsValidator.php
@@ -0,0 +1,149 @@
+<?php declare( strict_types=1 );
+
+namespace App\Services\Nfs;
+
+use Throwable;
+use App\Contracts\Runner;
+use Illuminate\Filesystem\Filesystem;
+use App\Exceptions\SystemExitException;
+
+/**
+ * Validate NFS is properly configured.
+ *
+ * @package App\Services\Nfs
+ */
+class NfsValidator {
+
+    public const NFS_CONF = '/etc/nfs.conf';
+
+    /**
+     * The Illuminate Filesystem.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * The command runner.
+     *
+     * @var \App\Contracts\Runner
+     */
+    protected $runner;
+
+    /**
+     * Controls the nfsd binary.
+     *
+     * @var \App\Services\Nfs\Nfsd
+     */
+    protected $nfsd;
+
+    /**
+     * Whether we should restart nfsd.
+     *
+     * @var bool
+     */
+    protected $restart = false;
+
+    /**
+     * Validator constructor.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $filesystem
+     * @param  \App\Contracts\Runner              $runner
+     * @param  \App\Services\Nfs\Nfsd             $nfsd
+     */
+    public function __construct( Filesystem $filesystem, Runner $runner, Nfsd $nfsd ) {
+        $this->filesystem = $filesystem;
+        $this->runner     = $runner;
+        $this->nfsd       = $nfsd;
+    }
+
+    /**
+     * Validate NFS Export Tables are configured correctly.
+     *
+     * @param  string  $exportsFilePath
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     * @throws \Symfony\Component\Process\Exception\ProcessFailedException
+     */
+    public function validate( string $exportsFilePath ): void {
+        $this->verifyExportsFileExists( $exportsFilePath )
+             ->verifyNfsReservedPort();
+    }
+
+    /**
+     * nfsd will not start without a config file. Creating the file triggers a nfsd start
+     * so give it time to start up.
+     *
+     * @param  string  $exportsFilePath
+     *
+     * @return \App\Services\Nfs\NfsValidator
+     * @throws \Symfony\Component\Process\Exception\ProcessFailedException
+     */
+    protected function verifyExportsFileExists( string $exportsFilePath ): self {
+        if ( $this->filesystem->exists( $exportsFilePath ) ) {
+            return $this;
+        }
+
+        $this->runner->with( [ 'path' => $exportsFilePath, ] )
+                     ->run( 'sudo touch {{ $path }}' )
+                     ->throw();
+
+        sleep( 5 );
+
+        return $this;
+    }
+
+    /**
+     * Ensure the NFS server has nfs.server.mount.require_resv_port = 0
+     * otherwise NFS mounts usually fail.
+     *
+     * @return $this
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     * @throws \Symfony\Component\Process\Exception\ProcessFailedException
+     */
+    protected function verifyNfsReservedPort(): self {
+        $line    = 'nfs.server.mount.require_resv_port = 0';
+        $content = $this->filesystem->get( self::NFS_CONF );
+
+        if ( ! str_contains( $content, $line ) ) {
+            $this->runner->with( [ 'content' => $line, 'path' => self::NFS_CONF ] )
+                         ->run( 'echo "{{ $content }}" | sudo tee -a {{ $path }}' )
+                         ->throw();
+            $this->restart = true;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Test the exports table on a temporary file. The thrown exception should contain
+     * the errors required to fix to continue.
+     *
+     * @param  string  $tempFile  The path to the temporary /etc/exports file to validate.
+     *
+     * @throws \App\Exceptions\SystemExitException
+     */
+    public function checkExports( string $tempFile ): void {
+        try {
+            $this->nfsd->check( $tempFile );
+        } catch ( Throwable $exception ) {
+            $this->filesystem->delete( $tempFile );
+            throw new SystemExitException( 'There was an error in the /etc/exports file when trying to set up the NFS share: ' . $exception->getMessage() );
+        }
+    }
+
+    /**
+     * See if we should restart the nfsd server.
+     */
+    public function __destruct() {
+        if ( ! $this->restart ) {
+            return;
+        }
+
+        $this->nfsd->restart();
+
+        sleep( 5 );
+    }
+
+}
