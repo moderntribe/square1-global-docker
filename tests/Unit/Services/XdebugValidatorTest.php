5a4e2f4c71400b7c80f2eac10aaeff4eb4fde748
defunctl <justin.frydman@gmail.com>
2021-04-10 16:50:07 -0600

    Move $path into property

--- tests/Unit/Services/XdebugValidatorTest.php
+++ tests/Unit/Services/XdebugValidatorTest.php
@@ -9,40 +9,39 @@ use App\Services\XdebugValidator;
 class XdebugValidatorTest extends TestCase {
 
     private $file;
+    private $path;
 
     protected function setUp(): void {
         parent::setUp();
 
         $this->file = $this->mock( FileIO::class );
+        $this->path = storage_path( 'tests/dev/docker/php/php-ini-overrides.ini' );
     }
 
     public function test_it_finds_a_valid_php_ini_file() {
-        $path      = storage_path( 'tests/dev/docker/php/php-ini-overrides.ini' );
         $validator = new XdebugValidator( $this->file );
 
-        $this->file->expects( 'exists' )->once()->with( $path )->andReturnTrue();
-        $this->file->expects( 'contains' )->once()->with( $path, 'xdebug.mode' )->andReturnTrue();
+        $this->file->expects( 'exists' )->once()->with( $this->path )->andReturnTrue();
+        $this->file->expects( 'contains' )->once()->with( $this->path, 'xdebug.mode' )->andReturnTrue();
 
-        $this->assertTrue( $validator->valid( $path ) );
+        $this->assertTrue( $validator->valid( $this->path ) );
     }
 
     public function test_it_returns_true_if_old_squareone_project() {
-        $path      = storage_path( 'tests/dev/docker/php/php-ini-overrides.ini' );
         $validator = new XdebugValidator( $this->file );
 
-        $this->file->expects( 'exists' )->once()->with( $path )->andReturnFalse();
+        $this->file->expects( 'exists' )->once()->with( $this->path )->andReturnFalse();
 
-        $this->assertTrue( $validator->valid( $path ) );
+        $this->assertTrue( $validator->valid( $this->path ) );
     }
 
     public function test_it_find_invalid_php_ini_configuration() {
-        $path      = storage_path( 'tests/dev/docker/php/php-ini-overrides.ini' );
         $validator = new XdebugValidator( $this->file );
 
-        $this->file->expects( 'exists' )->once()->with( $path )->andReturnTrue();
-        $this->file->expects( 'contains' )->once()->with( $path, 'xdebug.mode' )->andReturnFalse();
+        $this->file->expects( 'exists' )->once()->with( $this->path )->andReturnTrue();
+        $this->file->expects( 'contains' )->once()->with( $this->path, 'xdebug.mode' )->andReturnFalse();
 
-        $this->assertFalse( $validator->valid( $path ) );
+        $this->assertFalse( $validator->valid( $this->path ) );
     }
 
 }
