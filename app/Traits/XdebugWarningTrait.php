6e42f8166eb7d350b0b08d0cc61d70a91b7c6db7
defunctl <justin.frydman@gmail.com>
2021-04-10 17:12:58 -0600

    Add XdebugWarningTrait.php

---
+++ app/Traits/XdebugWarningTrait.php
@@ -0,0 +1,14 @@
+<?php declare( strict_types=1 );
+
+namespace App\Traits;
+
+trait XdebugWarningTrait {
+
+    protected function outdatedXdebugWarning( string $phpIni ): void {
+        $this->warn( sprintf(
+            'This project\'s %s is not configured correctly for xdebug v3.0+. See %s for more information.',
+            $phpIni,
+            'https://github.com/moderntribe/square-one/pull/695'
+        ) );
+    }
+}
