01dbb6a9930821e3cbc5667601e4034443cbf559
defunctl <justin.frydman@gmail.com>
2021-04-10 17:22:21 -0600

    Add comment

--- app/Traits/XdebugWarningTrait.php
+++ app/Traits/XdebugWarningTrait.php
@@ -4,6 +4,11 @@ namespace App\Traits;
 
 trait XdebugWarningTrait {
 
+    /**
+     * Display a warning in a Command if a user has an old configuration for Xdebug.
+     *
+     * @param  string  $phpIni The path to the project's docker php-ini-overrides.ini
+     */
     protected function outdatedXdebugWarning( string $phpIni ): void {
         $this->warn( sprintf(
             'This project\'s %s is not configured correctly for xdebug v3.0+. See %s upgrade instructions.',
