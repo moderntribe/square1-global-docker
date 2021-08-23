3416b846cb10447c3265e5adab29e30eff418ec6
Justin Frydman <justin.frydman@gmail.com>
2021-08-23 16:49:29 -0600

    First pass at debian flavor automatic installation

--- install/install.sh
+++ install/install.sh
@@ -19,7 +19,7 @@ install_homebrew() {
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 }
 
-enable_bash_autocomplete() {
+enable_autocomplete() {
   if [[ "$OSTYPE" == "darwin"* ]]; then
     sudo curl -s 'https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/squareone.autocompletion' -o $(brew --prefix)/etc/bash_completion.d/squareone.autocompletion
     curl -s 'https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/squareone.autocompletion.zsh' -o ~/.squareone_completion.zsh && echo "source ~/.squareone_completion.zsh" >> ~/.zshrc
@@ -80,10 +80,56 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
   echo "Installing dependencies via brew..."
   curl -s https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/brew/packages.txt -o ${CONFIG_DIR}/packages.txt
-  brew install $(<"${CONFIG_DIR}"/packages.txt)
+  brew install "$(<${CONFIG_DIR}/packages.txt)"
   echo "Setting the default PHP version to 7.4..."
   brew link php@7.4 --force
 fi
+
+# Debian Linux flavors
+if [[ -x "$(command -v apt-get)" ]]; then
+    echo "Installing dependencies via apt, enter your sudo password when requested..."
+
+    echo "Removing legacy docker installs..."
+    sudo apt-get remove docker docker-engine docker.io containerd runc
+
+    echo "Preparing docker sources..."
+    sudo apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+      "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    echo "Updating and upgrading apt..."
+    sudo apt-get update && sudo apt-get upgrade
+    curl -s https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/install/debian/apt.txt -o ${CONFIG_DIR}/apt.txt
+
+    echo "Installing docker-compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo curl \
+        -L https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/bash/docker-compose \
+        -o /etc/bash_completion.d/docker-compose
+
+    echo "Installing packages..."
+    sudo apt-get install -y "$(<${CONFIG_DIR}/apt.txt)"
+
+    echo "Backing up /etc/NetworkManager/NetworkManager.conf and creating a version that uses openresolv..."
+    sudo mv /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bak
+    sudo curl -s https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/install/debian/NetworkManager.conf -o /etc/NetworkManager/NetworkManager.conf
+
+    echo "Disabling systemd-resolved DNS service..."
+    sudo systemctl disable systemd-resolved
+    sudo systemctl stop systemd-resolved
+    sudo rm -rf /etc/resolv.conf
+    echo "Generating a new /etc/resolv.conf..."
+    sudo resolvconf -u
+fi
+
 
 echo "Installing nvm"
 install_nvm
@@ -92,7 +138,7 @@ echo "Creating config folder: ~/.config/squareone"
 create_config_folder
 
 echo "Enabling SquareOne autocompletion, enter your password when requested."
-enable_bash_autocomplete
+enable_autocomplete
 
 echo "Downloading so.phar to /usr/local/bin/so, enter your password when requested."
 install_phar
@@ -102,5 +148,12 @@ so
 
 echo ""
 echo "************************************"
-echo "If everything went smoothly, you should see the 'so' command list above. Reload your terminal to enable autocompletion."
+echo "If everything went smoothly, you should see the 'so' command list above. Reboot to properly complete installation."
 echo "************************************"
+echo "* Reboot now to complete the installation [y/n]?"
+read -r CHOICE
+if [[ $CHOICE == y* ]]; then
+    sudo reboot
+else
+    echo "* Done! Make sure you reboot to complete the installation."
+fi
