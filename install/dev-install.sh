b017f68f443ae27e7498772701b281fee63c1d33
Justin <defunctl@users.noreply.github.com>
2021-08-24 16:31:38 -0600

    Feature/install improvements (#85)
    * First pass at debian flavor automatic installation
    
    * Temporarily use this branch's path for testing
    
    * make notices clearer
    
    * install php-cli and not the full php package. Remove php-zlib since it doesn't exist
    
    * Read from apt.txt with xargs
    
    * Create config folder before doing other logic
    
    * update curl command for packages, don't quote apt path
    
    * Add resolv.conf.head + install
    
    * Fix docker permissions
    
    * Add mariadb-client to packages list
    
    * update README.md
    
    * switch back URL for when this is merged
    
    * Update dev-install.sh
    
    * Add libnss3-tools for when we'll improve SSL install on Debian
    
    * Install dc bash completion after packages are installed
    
    * bring dc bash completion update to dev install
    
    * automatically install composer

--- install/dev-install.sh
+++ install/dev-install.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
 #############################################################
-# SquareOne Global Docker Dev Installer
+# SquareOne Global Docker Installer
 #############################################################
 
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
-
+CONFIG_DIR=~/.config/squareone
 
 # Functions
 create_config_folder() {
-  mkdir -p ~/.config/squareone
+  if [ ! -d "${CONFIG_DIR}" ]; then
+    mkdir -p ${CONFIG_DIR}
+  fi
 }
 
 install_homebrew() {
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 }
 
-enable_bash_autocomplete() {
+enable_autocomplete() {
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    sudo cp -f ${SCRIPTDIR}/../squareone.autocompletion $(brew --prefix)/etc/bash_completion.d/squareone.dev.autocompletion
-    cp -f ${SCRIPTDIR}/../squareone.autocompletion.zsh ~/.squareone_dev_completion.zsh && echo "source ~/.squareone_dev_completion.zsh" >> ~/.zshrc
+    sudo curl -s 'https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/squareone.autocompletion' -o $(brew --prefix)/etc/bash_completion.d/squareone.dev.autocompletion
+    curl -s 'https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/squareone.autocompletion.zsh' -o ~/.squareone_dev_completion.zsh && echo "source ~/.squareone_dev_completion.zsh" >> ~/.zshrc
   else
-    sudo cp -f ${SCRIPTDIR}/../squareone.autocompletion /etc/bash_completion.d/squareone.dev.autocompletion
-    cp -f ${SCRIPTDIR}/../squareone.autocompletion.fish ~/.config/fish/completions/so.dev.fish
+    sudo curl -s 'https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/squareone.autocompletion' -o /etc/bash_completion.d/squareone.dev.autocompletion
+    curl -s 'https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/squareone.autocompletion.fish' -o ~/.config/fish/completions/so.dev.fish
   fi
 }
 
@@ -33,6 +35,9 @@ symlink_sq1() {
 install_nvm() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 }
+
+echo "Creating config folder: ~/.config/squareone"
+create_config_folder
 
 # OSX
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -54,19 +59,70 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   fi
 
   echo "Installing dependencies via brew..."
-  brew install $(<brew/packages.txt)
+  curl -s https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/brew/packages.txt -o ${CONFIG_DIR}/packages.txt
+  brew install "$(<${CONFIG_DIR}/packages.txt)"
   echo "Setting the default PHP version to 7.4..."
   brew link php@7.4 --force
+fi
+
+# Debian Linux flavors
+if [[ -x "$(command -v apt-get)" ]]; then
+    echo "* Installing dependencies via apt, enter your sudo password when requested..."
+
+    echo "* Removing legacy docker installs..."
+    sudo apt-get remove docker docker-engine docker.io containerd runc
+
+    echo "Preparing docker sources..."
+    sudo apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+      "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    echo "* Updating and upgrading apt..."
+    sudo apt-get update -y && sudo apt-get upgrade -y
+    curl -fsSL https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/install/debian/apt.txt -o ${CONFIG_DIR}/apt.txt
+
+    echo "* Installing docker-compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
+    echo "* Installing packages..."
+    xargs -a ${CONFIG_DIR}/apt.txt sudo apt-get install -y
+
+    echo "* Installing docker-compose bash completion"
+    sudo curl \
+        -L https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/bash/docker-compose \
+        -o /etc/bash_completion.d/docker-compose
+
+    echo "* Installing nameservers to /etc/resolv.conf.head..."
+    sudo curl -fsSL https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/install/debian/resolv.conf.head -o /etc/resolv.conf.head
+
+    echo "* Backing up /etc/NetworkManager/NetworkManager.conf and creating a version that uses openresolv..."
+    sudo mv /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bak
+    sudo curl -s https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/install/debian/NetworkManager.conf -o /etc/NetworkManager/NetworkManager.conf
+
+    echo "* Disabling systemd-resolved DNS service..."
+    sudo systemctl disable systemd-resolved
+    sudo systemctl stop systemd-resolved
+    sudo rm -rf /etc/resolv.conf
+    echo "* Generating a new /etc/resolv.conf..."
+    sudo resolvconf -u
+
+    echo "* Fixing docker permissions"
+    sudo usermod -a -G docker "$USER"
 fi
 
 echo "Installing nvm"
 install_nvm
 
-echo "Creating config folder: ~/.config/squareone"
-create_config_folder
-
 echo "Enabling SquareOne autocompletion, enter your password when requested."
-enable_bash_autocomplete
+enable_autocomplete
 
 echo "Running composer install..."
 composer install -d ${SCRIPTDIR}/../
@@ -78,5 +134,12 @@ sodev
 
 echo ""
 echo "************************************"
-echo "If everything went smoothly, you should see the so command list above. Reload your terminal to enable autocompletion."
+echo "If everything went smoothly, you should see the 'sodev' command list above. Reboot to properly complete installation."
 echo "************************************"
+echo "* Reboot now to complete the installation [y/n]?"
+read -r CHOICE
+if [[ $CHOICE == y* ]]; then
+    sudo reboot
+else
+    echo "* Done! Make sure you reboot to complete the installation."
+fi
--- install/install.sh
+++ install/install.sh
@@ -19,7 +19,7 @@ install_homebrew() {
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 }
 
-enable_bash_autocomplete() {
+enable_autocomplete() {
   if [[ "$OSTYPE" == "darwin"* ]]; then
     sudo curl -s 'https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/squareone.autocompletion' -o $(brew --prefix)/etc/bash_completion.d/squareone.autocompletion
     curl -s 'https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/squareone.autocompletion.zsh' -o ~/.squareone_completion.zsh && echo "source ~/.squareone_completion.zsh" >> ~/.zshrc
@@ -59,6 +59,9 @@ install_nvm() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 }
 
+echo "Creating config folder: ~/.config/squareone"
+create_config_folder
+
 # OSX
 if [[ "$OSTYPE" == "darwin"* ]]; then
   command -v docker >/dev/null 2>&1
@@ -80,19 +83,69 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
   echo "Installing dependencies via brew..."
   curl -s https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/brew/packages.txt -o ${CONFIG_DIR}/packages.txt
-  brew install $(<"${CONFIG_DIR}"/packages.txt)
+  brew install "$(<${CONFIG_DIR}/packages.txt)"
   echo "Setting the default PHP version to 7.4..."
   brew link php@7.4 --force
+fi
+
+# Debian Linux flavors
+if [[ -x "$(command -v apt-get)" ]]; then
+    echo "* Installing dependencies via apt, enter your sudo password when requested..."
+
+    echo "* Removing legacy docker installs..."
+    sudo apt-get remove docker docker-engine docker.io containerd runc
+
+    echo "Preparing docker sources..."
+    sudo apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+      "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    echo "* Updating and upgrading apt..."
+    sudo apt-get update -y && sudo apt-get upgrade -y
+    curl -fsSL https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/install/debian/apt.txt -o ${CONFIG_DIR}/apt.txt
+
+    echo "* Installing docker-compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
+    echo "* Installing packages..."
+    xargs -a ${CONFIG_DIR}/apt.txt sudo apt-get install -y
+
+    echo "* Installing docker-compose bash completion"
+    sudo curl \
+        -L https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/bash/docker-compose \
+        -o /etc/bash_completion.d/docker-compose
+
+    echo "* Installing nameservers to /etc/resolv.conf.head..."
+    sudo curl -fsSL https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/install/debian/resolv.conf.head -o /etc/resolv.conf.head
+
+    echo "* Backing up /etc/NetworkManager/NetworkManager.conf and creating a version that uses openresolv..."
+    sudo mv /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bak
+    sudo curl -s https://raw.githubusercontent.com/moderntribe/square1-global-docker/master/install/debian/NetworkManager.conf -o /etc/NetworkManager/NetworkManager.conf
+
+    echo "* Disabling systemd-resolved DNS service..."
+    sudo systemctl disable systemd-resolved
+    sudo systemctl stop systemd-resolved
+    sudo rm -rf /etc/resolv.conf
+    echo "* Generating a new /etc/resolv.conf..."
+    sudo resolvconf -u
+
+    echo "* Fixing docker permissions"
+    sudo usermod -a -G docker "$USER"
 fi
 
 echo "Installing nvm"
 install_nvm
 
-echo "Creating config folder: ~/.config/squareone"
-create_config_folder
-
 echo "Enabling SquareOne autocompletion, enter your password when requested."
-enable_bash_autocomplete
+enable_autocomplete
 
 echo "Downloading so.phar to /usr/local/bin/so, enter your password when requested."
 install_phar
@@ -102,5 +155,12 @@ so
 
 echo ""
 echo "************************************"
-echo "If everything went smoothly, you should see the 'so' command list above. Reload your terminal to enable autocompletion."
+echo "If everything went smoothly, you should see the 'so' command list above. Reboot to properly complete installation."
 echo "************************************"
+echo "* Reboot now to complete the installation [y/n]?"
+read -r CHOICE
+if [[ $CHOICE == y* ]]; then
+    sudo reboot
+else
+    echo "* Done! Make sure you reboot to complete the installation."
+fi
