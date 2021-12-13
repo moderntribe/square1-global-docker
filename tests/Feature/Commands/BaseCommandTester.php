afec2530cb7cb33d6e812671cfffa1fbe2f4905f
Justin Frydman <justin.frydman@gmail.com>
2021-12-13 14:18:32 -0700

    Allow testing of symfony commands in addition to laravel commands

--- tests/Feature/Commands/BaseCommandTester.php
+++ tests/Feature/Commands/BaseCommandTester.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Feature\Commands;
 
-use Tests\TestCase;
+use Illuminate\Console\Application;
 use LaravelZero\Framework\Commands\Command;
-use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Component\Console\Application as ConsoleApplication;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tests\TestCase;
 
 /**
  * Include a command running method to test commands.
@@ -22,20 +23,25 @@ abstract class BaseCommandTester extends TestCase {
      * The `CommandTester` is directly returned, use methods like
      * `->getDisplay()` or `->getStatusCode()` on it.
      *
-     * @param  Command  $command
-     * @param  array    $arguments         The command line arguments, array of key=>value
-     *                                     Examples:
-     *                                     - named  arguments: ['model' => 'Post']
-     *                                     - boolean flags: ['--all' => true]
-     *                                     - arguments with values: ['--arg' => 'value']
-     * @param  array    $interactiveInput  Interactive responses to the command
-     *                                     I.e. anything the command `->ask()` or `->confirm()`, etc.
+     * @param  Command|\Symfony\Component\Console\Command\Command  $command
+     * @param  array                                               $arguments         The command line arguments, array of key=>value
+     *                                                                                Examples:
+     *                                                                                - named  arguments: ['model' => 'Post']
+     *                                                                                - boolean flags: ['--all' => true]
+     *                                                                                - arguments with values: ['--arg' => 'value']
+     * @param  array                                               $interactiveInput  Interactive responses to the command
+     *                                                                                I.e. anything the command `->ask()` or `->confirm()`, etc.
      *
      * @return CommandTester
      */
-    protected function runCommand( Command $command, array $arguments = [], array $interactiveInput = [] ): CommandTester {
-        $command->setApplication( new ConsoleApplication() );
-        $command->setLaravel( $this->app );
+    protected function runCommand( $command, array $arguments = [], array $interactiveInput = [] ): CommandTester {
+        if ( method_exists( $command, 'setLaravel' ) ) {
+            $command->setApplication( new ConsoleApplication() );
+            $command->setLaravel( $this->app );
+        } else {
+            // This is a symfony command
+            $command->setApplication( $this->app->make( Application::class, [ 'version' => '1.0.0' ] ) );
+        }
 
         $tester = new CommandTester( $command );
         $tester->setInputs( $interactiveInput );
