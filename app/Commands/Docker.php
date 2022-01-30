504cc88ed1b35d0becdaacc4f80477709a42d3c8
Justin Frydman <justin.frydman@gmail.com>
2022-01-30 16:12:42 -0700

    Refactor argument version rewriting, add logic to pass proper --user user:group arguments to docker exec

--- app/Commands/Docker.php
+++ app/Commands/Docker.php
@@ -4,6 +4,7 @@ namespace App\Commands;
 
 use App\Contracts\ArgumentRewriter;
 use App\Contracts\Runner;
+use App\Factories\ParameterFactory;
 use App\Recorders\ResultRecorder;
 use App\Services\Docker\Local\Config;
 use App\Traits\ArgumentRewriterTrait;
@@ -31,8 +32,20 @@ class Docker extends BaseCommand implements ArgumentRewriter {
      */
     protected $description = 'Pass through for the docker binary';
 
-    public function __construct() {
+    /**
+     * Creates our Parameter Manager Object.
+     *
+     * @var \App\Factories\ParameterFactory
+     */
+    protected $factory;
+
+    /**
+     * @param  \App\Factories\ParameterFactory  $factory
+     */
+    public function __construct( ParameterFactory $factory ) {
         parent::__construct();
+
+        $this->factory = $factory;
 
         // Allow this command to receive any options/arguments
         $this->ignoreValidationErrors();
@@ -47,29 +60,41 @@ class Docker extends BaseCommand implements ArgumentRewriter {
      * @return int
      */
     public function handle( Runner $runner, ResultRecorder $recorder ): int {
-        // Get the entire input passed to this command.
-        $command = (string) $this->input;
+        $input = $this->factory->make( $this->input );
 
-        // Replace version options and flags
-        $command = $this->restoreVersionArguments( $command );
+        // Ensure executed commands are run by the host user
+        if ( ! $input->has( [ '--user' ] ) ) {
+            $user = $input->add( [
+                '--user',
+                sprintf( '%s:%s', Config::uid(), Config::gid() ),
+            ], 'exec' );
+
+            if ( ! $user ) {
+                $this->error( 'Unable to assign user permissions to docker exec' );
+
+                return self::FAILURE;
+            }
+        }
 
         $tty = false;
 
-        if ( str_contains( $command, '-t' ) || str_contains( $command, '--tty' ) ) {
+        if ( $input->has( [ '-t', '--tty' ] ) ) {
             $tty = true;
         }
 
-        // Force all docker exec commands to be interactive so the user can provide input, if required
-        if ( str_contains( $command, 'exec' ) && ! str_contains( $command, ' -i ' ) && ! str_contains( $command, ' --interactive ' ) ) {
-            $command = str_replace( 'exec', 'exec -i', $command );
+        // Force all docker exec commands to be interactive so the user can provide input, if required.
+        if ( ! $input->has( [ '-i', '--interactive' ] ) ) {
+            $input->add( [ '--interactive' ], 'exec' );
         }
+
+        // Add back in --version, -V if required
+        $this->restoreVersionArguments( $input );
+
+        // Convert the entire command to a string
+        $command = (string) $input;
 
         $response = $runner->output( $this )
                            ->tty( $tty )
-                           ->withEnvironmentVariables( [
-                               Config::ENV_UID => Config::uid(),
-                               Config::ENV_GID => Config::gid(),
-                           ] )
                            ->run( $command );
 
         $recorder->add( $response->process()->getOutput() );
