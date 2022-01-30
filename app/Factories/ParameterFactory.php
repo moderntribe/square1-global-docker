b7bff14aff185197b91115fc637cc38cfd1e9a04
Justin Frydman <justin.frydman@gmail.com>
2022-01-30 16:11:38 -0700

    Add ParameterFactory.php

---
+++ app/Factories/ParameterFactory.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace App\Factories;
+
+use App\Input\ParameterManager;
+use Illuminate\Contracts\Foundation\Application;
+use Symfony\Component\Console\Input\InputInterface;
+
+class ParameterFactory {
+
+    /**
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    public function __construct( Application $app ) {
+        $this->app = $app;
+    }
+
+    public function make( InputInterface $input ): ParameterManager {
+        return $this->app->make( ParameterManager::class, [ 'input' => $input ] );
+    }
+
+}
